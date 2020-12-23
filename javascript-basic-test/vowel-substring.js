da4550f867e1691faaa0b0115cb666b4168c5e0f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-22 21:41:24 -0300

    Vowel Substrings

--- javascript-basic-test/vowel-substring.js
+++ javascript-basic-test/vowel-substring.js
@@ -1,3 +1,14 @@
 function findSubstrings(s, k) {
-  
+  let string = s;
+  const counter = s.length - k;
+  const arrayWithSubStrings = [];
+  for (let index = 0; index <= counter; index += 1) {
+    let subString = string.slice(index, k + index);
+    arrayWithSubStrings.push(subString);
+  }
+  return arrayWithSubStrings;
 }
+
+console.log(findSubstrings('azerdii', 5));
+
+console.log(findSubstrings('qwdftr', 2));
