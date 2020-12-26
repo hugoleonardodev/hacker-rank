a227218a1fd3b687ebe25e742ce053a733ddde47
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-26 19:59:57 -0300

    Classes and Inheritance

--- problem-solving/tempCodeRunnerFile.js
+++ problem-solving/tempCodeRunnerFile.js
@@ -1,18 +1,5 @@
-
-class Polygon {
-  constructor(sidesLengthArray) {
-    this.polygon = sidesLengthArray
-    this.perimeter = perimeter;
-  }
-}
-
-function perimeter() {
-  return this.polygon.reduce((acc, cur) => {
-    acc = acc + cur;
-    return acc;
-  });
-}
-
-let triangle = new Polygon([3, 4, 5]);
-
-console.log(triangle.perimeter());
+class Square extends Rectangle {
+//   area() {
+//     return (this.w)**2; 
+//   }
+// }
