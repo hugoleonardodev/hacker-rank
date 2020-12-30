d632a58ab3b1bd4b85ebb1e155d3d97dbd4377be
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-30 02:16:44 -0300

    Happy Lady Bug

---
+++ problem-solving/basic/implementation/happyLadyBugs.js
@@ -0,0 +1,23 @@
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const regex = /\A\A+/g;
+
+function happyLadyBus(b) {
+  if (b.includes('_')) {
+    const arr = b.replace('_', '').split('').sort();
+    const subArr = []
+    let counter = arr.length;
+    while (counter > 0) {
+      let bug = arr[0];
+      let subArrLength = arr.filter(bugs => bugs === bug).length;
+      subArr.push(arr.filter(bugs => bugs === bug));
+      arr.splice(0, arr.filter(a=>a===arr[0]).length);
+      counter -= subArrLength;
+    }
+    console.log(subArr);
+  }
+}
+
+// console.log(happyLadyBus('AABBC'));
+
+console.log(happyLadyBus('AAABBBC_CCD'));
